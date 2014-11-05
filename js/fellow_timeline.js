35faa2b35653aa6debfdf44b9f4021f6512f4a7e
nenegesi <nenegesi@gmail.com>
2014-11-05 17:35:51 +0100

    fellow timelines

---
+++ js/fellow_timeline.js
@@ -0,0 +1,20 @@
+var fellowTimeline = {
+ 
+    init: function() {
+        fellowTimeline.createStory();
+    },
+
+    createStory: function(){
+        createStoryJS({
+            type:       'timeline',
+            width:      '1270',
+            height:     '650',
+            source:     timelineData,
+            embed_id:   'fellow-timeline',
+            start_zoom_adjust: 3
+        });
+    }
+ 
+};
+ 
+$(document).ready(fellowTimeline.init);
